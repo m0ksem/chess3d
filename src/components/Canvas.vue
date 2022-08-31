76d643bfa1b14bf80fee3d0a4807ed149f003670
m0ksem <m0ksem1337@gmail.com>
2022-08-31 16:17:07 +0300

    feat: replace pawn with 3d rjakij

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-  import { WebGLRenderer, PerspectiveCamera, Scene, AmbientLight, Plane } from 'three'
-  import Table from '../assets/models/table.obj?three'
-  import Pawn from '../assets/models/pawn/pawn.obj?three'
+  import { WebGLRenderer, PerspectiveCamera, Scene, AmbientLight, Mesh, MeshBasicMaterial } from 'three'
+  import table from '../assets/models/table.obj?three'
+  import chibi from '../assets/models/chibi.obj?three'
 
   const canvas = ref<HTMLCanvasElement>()
 
-  const movePawn = (pawn: typeof Pawn, x: number, z: number) => {
-    // normalize
-    pawn.position.x = 1/4 * -4 + 1/8
-    pawn.position.z = 1/4 * 3 + 1/8
-    // Move
-    pawn.position.x += x * 1/4
-    pawn.position.z += z * -1/4
+  const movePawn = (pawn: typeof chibi, x: number, z: number) => {
+    // normalize: move to top left and center on cell
+    pawn.position.x = -4 + 0.5
+    pawn.position.z = -4 + 0.5
+    pawn.position.x += x
+    pawn.position.z += z
   }
 
   onMounted(() => {
@@ -23,22 +22,32 @@
     const renderer = new WebGLRenderer({ canvas: canvas.value, alpha: true })
 
     const camera = new PerspectiveCamera(75, canvas.value.width / canvas.value.height, 1, 1000)
-    camera.position.z = 2
-    camera.position.y = 2
+    camera.position.set(0, 5, 5)
     camera.lookAt(0, 0, 0)
+
+    // 4, but we have borders so 4.2
+    table.scale.set(4.2, 4.2, 4.2)
 
     const scene = new Scene()
 
-    Table.scale.set(0.25, 0.25, 0.25)
-
+    const whitePawnMaterial = new MeshBasicMaterial({ color: 0xeeeeee })
     for (let i = 0; i < 8; i++) {
-      const pawn = Pawn.clone()
+      const pawn = chibi.clone(true)
+      pawn.children.forEach((c) => (c as Mesh).material = whitePawnMaterial)
       scene.add(pawn)
       movePawn(pawn, i, 1)
     }
+
+    const blackPawnMaterial = new MeshBasicMaterial({ color: 0x222222 })
+    for (let i = 0; i < 8; i++) {
+      const pawn = chibi.clone()
+      pawn.children.forEach((c) => (c as Mesh).material = blackPawnMaterial)
+      scene.add(pawn)
+      movePawn(pawn, i, 6)
+    }
     
     scene
-      .add(Table)
+      .add(table)
       .add(new AmbientLight(0xffffff, 2))
 
     const render = () => {
